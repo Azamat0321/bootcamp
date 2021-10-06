0cff84f51603f6eed6111b27b6840f084633d71f
Azamat0321 <dhxndb@gmail.com>
2021-10-07 00:45:23 +0500

    Lab 5 qoshdim

---
+++ Solutions/Lab5.cs
@@ -0,0 +1,12 @@
+using System.Net.WebSockets;
+using System.Runtime.ConstrainedExecution;
+namespace bootcamp.Solutions
+{
+    public class Lab5
+    {
+        public void Problem1()
+        {
+            Console.WriteLine("Problem1");    
+        }
+    }
+}
