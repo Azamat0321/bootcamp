40b7d6c36dca6af954827a6c743319c165126e62
Azamat0321 <dhxndb@gmail.com>
2021-10-07 17:06:30 +0500

    Lab5,6

--- Program.cs
+++ Program.cs
@@ -1,12 +1,106 @@
 ﻿using System;
-
-namespace bootcamp
+using System.Linq;
+using lesson2.Solutions;
+namespace lesson2
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            var lab2 = new Lab2();
+            {
+                lab2.Problem1();
+                lab2.Problem2();
+                lab2.Problem3();
+                lab2.Problem4();
+                lab2.Problem5();
+                lab2.Problem7();
+                lab2.Problem8();
+                lab2.Problem9();
+            }
         }
+
+        static void Main3()
+        {
+            var lab3 = new Lab3();
+            {
+                lab3.Problem3_2();
+                lab3.Problem3_3();
+                lab3.Problem3_4();
+                lab3.Problem3_2_2();
+                lab3.Problem3_1();
+            }
+        }
+
+        static void Main4()
+        {
+            var lab4 = new Lab4();
+            {
+                lab4.Problem1();
+                lab4.Problem2();
+                lab4.Problem3();
+                lab4.Problem4();
+                lab4.Problem5();
+                lab4.Problem6();
+                lab4.Problem7();
+                lab4.Problem8();
+                lab4.Problem9();
+                lab4.Problem10();
+            
+            }
+        }
+
+        static void Main6()
+        {
+            var lab6 = new Lab6();
+            lab6.Problem6_1();
+            lab6.Problem6_2();
+            lab6.Problem6_3();
+            lab6.Problem6_4();
+            lab6.Problem6_5();
+            lab6.Problem6_6();
+            lab6.Problem6_7();
+            lab6.Problem6_8();
+            lab6.Problem6_9();
+            lab6.Problem6_11();
+            lab6.Problem6_12();
+            lab6.Problem6_13();
+            lab6.Problem6_14();
+            lab6.Problem6_15();
+            lab6.Problem6_16();
+            lab6.Problem6_17();
+            lab6.Problem6_19();
+            lab6.Problem6_20();
+            lab6.Problem6_21();
+            lab6.Problem6_22();
+            lab6.Problem6_23();
+            lab6.Problem6_24();
+        
+        }
+        static void Main5()
+        {
+            var lab5 = new Lab5();
+            lab5.Problem1();
+            lab5.Problem2();
+            lab5.Problem3();
+            lab5.Problem4();
+            lab5.Problem5();
+            lab5.Problem6();
+            lab5.Problem7();
+            lab5.Problem8();
+            lab5.Problem9();
+            lab5.Problem10();
+            lab5.Problem11();
+            lab5.Problem12();
+            lab5.Problem13();
+            lab5.Problem14();
+            lab5.Problem15();
+            lab5.Problem16();
+            lab5.Problem17();
+            lab5.Problem18();
+            lab5.Problem19();
+            lab5.Problem20();
+        }
+
     }
 }
